a04846e0639323842bbdd202cbd00ed9b0c542dd
Tim Strickler <tsstric@pointpark.edu>
2019-07-27 12:44:00 -0400

    Created a shell of the webserver.

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+var http = require('http');
+var fs = require('fs');
+var mysql = require('mysql');
+
+app.set('port', process.env.PORT || 3001);
+
+
+
+
+
+
+//404 page
+app.use(function (req, res) {
+	res.type('text/plain');
+	res.status(404);
+	res.send('404 - Not Found');
+});
+
+//500 page
+app.use(function (err, req, res, next) {
+	console.error(err.stack);
+	res.type('text/plain');
+	res.status(500);
+	res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Express started on http://localhost: ' +
+		app.get('port') + '; press Ctrl-C to terminate.');
+});
