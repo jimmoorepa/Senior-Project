22968f97a811e1ccf6235bba9781dc4e4877aca7
Jim Moore <jmoore@pointpark.edu>
2019-08-10 23:40:33 -0400

    New version of site using EJS

---
+++ Ejs/db.js
@@ -0,0 +1,15 @@
+var db = {
+	database: {
+		host:		'db.it.pointpark.edu',
+		user:		'FoodCoop',
+		password:	'FoodCoop',
+		port: 	  	'3306', 		// default MySQL port
+		database:	'FoodCoop'
+	},
+	server: {
+		host: 'jmoore.it.pointpark.edu',
+		port: '3000'
+	}
+}
+
+module.exports = db
